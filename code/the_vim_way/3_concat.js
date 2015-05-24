1c93259250175b765035076563525da8b66bb651
wangyiran <wangyiran125@icloud.com>
2015-05-24 16:58:30 +0800

    add commend description

--- code/the_vim_way/3_concat.js
+++ code/the_vim_way/3_concat.js
@@ -6,4 +6,35 @@
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/dnvim for more book information.
 ***/
+
+
+but we could make this a
+little easier on the eye by padding each + sign with spaces to make it look like this:
+
+Keystrokes
+Buffer Contents
+{start}
+var foo = "method("+argument1+","+argument2+")";
+
+f+   The f{char} command tells Vim to look
+ahead for the next occurrence of the specified character and then move the cursor
+directly to it if a match is found (see f ).
+var foo = "method("+argument1+","+argument2+")";
+
+s?+?<Esc>  The s command compounds two steps into one: it deletes the character under the
+cursor and then enters Insert mode.
+
+var foo = "method(" +_argument1+","+argument2+")";
+;   The ; command will repeat the last
+search that the f command performed.
+
+var foo = "method(" + argument1+","+argument2+")";
+.   we can repeat the
+change with the dot command;
+var foo = "method(" + argument1 +_","+argument2+")";
+;.
+var foo = "method(" + argument1 + "," +_argument2+")";
+;.
+
+
 var foo = "method("+argument1+","+argument2+")";
