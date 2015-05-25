8d2c24d60624d95ecffa78a1728bf7ff0ea3795a
wangyiran <wangyiran125@icloud.com>
2015-05-25 16:12:49 +0800

    add commend description

--- code/the_vim_way/2_foo_bar.js
+++ code/the_vim_way/2_foo_bar.js
@@ -6,6 +6,14 @@
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/dnvim for more book information.
 ***/
+
+Three consecutive lines, each of different length. We want to append a semicolon at
+the end of each
+we extend our selection to the end of each line
+by pressing $.
+we can append at the end of each line using the A
+command
+
 var foo = 1
 var bar = 'a'
 var foobar = foo + bar
